ee438b2c5e309508a790fd9973a475fd9c36b462
AnnZeller <annazeller@mail.ru>
2020-07-21 14:27:39 +0300

    Updates

---
+++ precache-manifest.c6e13e2fe1b4a1fee4dd8a1ccae6266f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd12b008a6ed7feb7021b7d81a25706",
+    "url": "/redux-sandbox/index.html"
+  },
+  {
+    "revision": "56946faebb9164559869",
+    "url": "/redux-sandbox/static/js/2.8696e6a3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-sandbox/static/js/2.8696e6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d042f23e8ef24ab8b671",
+    "url": "/redux-sandbox/static/js/main.2cab1298.chunk.js"
+  },
+  {
+    "revision": "3dfe9934e4057b4aaa07",
+    "url": "/redux-sandbox/static/js/runtime-main.167b004e.js"
+  }
+]);
